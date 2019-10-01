5b7587fba4820529bb852ba767036b59d273d3ef
msbb <mail@maximbuur.nl>
2019-10-01 10:59:21 +0200

    Modify menus route to accept parameters; returns warning

--- menu-table/routes.js
+++ menu-table/routes.js
@@ -1,46 +1,55 @@
-const { Router } = require("express")
-const Menu = require("./model")
+const { Router } = require("express");
+const Menu = require("./model");
 const router = new Router();
 
 //get menus by date
 router.get("/menus", (req, res) => {
-  const { date } = req.query
-  Menu
-    .findAll({
+  if (
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.data
+  ) {
+    const date = req.body.queryResult.parameters.data;
+
+    Menu.findAll({
       where: {
         date: new Date(date)
       }
-    })
-    .then(menu => {
-      res.json(menu)
-    })
-})
+    }).then(menu => {
+      res.json(menu);
+    });
+  } else {
+    return res.status(400).send({
+      message: "Please provide a date parameter",
+      succes: false
+    });
+  }
+});
 
 //create new menu
 router.post("/menus", (req, res) => {
-  const { dish } = req.body
-  Menu.create(dish)
-    .then(menu => res.status(201).json(menu))
-})
+  const { dish } = req.body;
+  Menu.create(dish).then(menu => res.status(201).json(menu));
+});
 
 //delete menu by id
 router.delete("/menus/:id", (req, res, next) => {
-  const { id } = req.params
-  console.log("REQ PARAMS", req.params)
-  Menu
-    .findByPk(id)
+  const { id } = req.params;
+  console.log("REQ PARAMS", req.params);
+  Menu.findByPk(id)
     .then(menuItem => {
       if (!menuItem) {
         return res.status(404).send({
           message: `Menu item does not exist`
-        })
+        });
       }
-      return menuItem.destroy()
-        .then(() => res.send({
+      return menuItem.destroy().then(() =>
+        res.send({
           message: `Menu item was deleted`
-        }))
+        })
+      );
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
-module.exports = router
+module.exports = router;

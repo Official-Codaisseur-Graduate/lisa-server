c011bc81693c46507d82e27708342af4eece28a8
pittvandewitt <thomasdewitt@outlook.com>
2019-10-25 13:14:46 +0200

    Refactor code, set location only once

--- google/routes.js
+++ google/routes.js
@@ -1,25 +1,20 @@
 const { Router } = require('express');
-
 const { findNearest } = require('geolib');
+const { dialogflow, Permission } = require('actions-on-google');
+
 const Menu = require('../menu-table/model');
 const Location = require('../location-table/model')
+
 const noTypeSentence = require('./googleFunctions/noType');
 const typeSentence = require('./googleFunctions/voorOfNagerecht');
 const hoofdgerechtSentence = require('./googleFunctions/hoofdgerechtSentence')
 
 const router = new Router();
-
-const {
-  dialogflow,
-  Permission
-} = require('actions-on-google');
-
-const app = dialogflow({
-  debug: true
-});
+const app = dialogflow(
+  //{ debug: true }
+);
 
 app.intent('fetch_menu', async (conv) => {
-  conv.user.storage.parameters = conv.body.queryResult.parameters;
   conv.data.requestedPermission = 'DEVICE_PRECISE_LOCATION'
   if (conv.user.storage.location) {
     await readMenu(conv)
@@ -36,17 +31,19 @@ app.intent('user_info', async (conv, params, permissionGranted) => {
   if (permissionGranted) {
     const { requestedPermission } = conv.data;
     if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
-      conv.user.storage.location = conv.device.location.coordinates
+      conv.user.storage.location =
+        await getLocation(conv.device.location.coordinates)
       await readMenu(conv)
     }
   } else {
-    return conv.close('Zonder locatie kan ik niet weten aan welke keuken ik de vraag moet stellen');
+    return conv.close('Zonder locatie kan ik niet weten aan' +
+      ' welke keuken ik de vraag moet stellen');
   }
 });
 
 app.intent('delete_location', (conv) => {
   delete conv.user.storage.location
-  conv.close('Oké, ik zal de volgende keer opnieuw de locatie opvragen')
+  conv.close('Oké, ik vraag straks opnieuw de locatie op')
 })
 
 app.intent('close_conversation', (conv) => {
@@ -54,60 +51,48 @@ app.intent('close_conversation', (conv) => {
 })
 
 app.fallback((conv) => {
-  conv.ask('Ik kan dit niet beantwoorden. Wat is uw vraag?')
+  conv.ask('Ik heb geen antwoord op deze vraag')
 })
 
 app.catch((conv, error) => {
   console.error(error)
-  conv.close('Er ging iets mis. Ik heb de ontwikkelaar huisarrest gegeven')
+  conv.close('Er ging iets mis. ' +
+    'Waarschijnlijk is er geen verbinding met de server')
 })
 
 //get menus by date for Google
 router.post('/google-menus', app)
 
 readMenu = async (conv) => {
-  const { id, name } = await getLocation(conv)
-
+  const { id, name } = conv.user.storage.location
   const body = conv.user.storage.parameters;
-
-  let date;
-  if (body.date && body.date.startDateTime) {
-    date = new Date(body.date.startDateTime);
-  } else if (body.date) {
-    date = new Date(body.date);
-  } else {
-    date = new Date();
-  }
-
+  const date = body.date && body.date.startDateTime ?
+    new Date(body.date.startDateTime) : body.date ?
+      new Date(body.date) : new Date()
   const menuExists = await Menu.findOne({
-    where: {
-      date,
-      locationId: id
-    }
+    where: { date, locationId: id }
   })
 
   if (!menuExists) {
-    const noMenu = `Het menu voor die dag bij ${name} is onbekend. Probeer een andere dag.`
-    return conv.ask(`${noMenu}`)
+    return conv.ask(`Het menu voor die dag bij ${name}` +
+      'is onbekend. Probeer een andere dag')
   }
 
-  let menu;
-  if (body.type.length === 0 || !body.type) {
-    menu = await noTypeSentence(id, date);
-  } else if (
-    body.type === 'Voorgerecht' ||
-    body.type === 'Nagerecht'
-  ) {
-    menu = await typeSentence(id, date, body.type);
-  } else if (
-    body.type === 'Hoofdgerecht'
-  ) {
-    menu = await hoofdgerechtSentence(id, date)
+  switch (body.type) {
+    case 'Voorgerecht':
+    case 'Nagerecht':
+      conv.ask(await typeSentence(id, date, body.type));
+      break;
+    case 'Hoofdgerecht':
+      conv.ask(await hoofdgerechtSentence(id, date));
+      break;
+    default:
+      conv.ask(await noTypeSentence(id, date));
+      break;
   }
-  return conv.ask(menu)
 }
 
-getLocation = async (conv) => {
+getLocation = async (coordinates) => {
   const locations = await Location.findAll()
   const vitalisLocations = [...locations.map(vitalisLocation =>
     vitalisLocation = {
@@ -116,7 +101,7 @@ getLocation = async (conv) => {
     }
   )]
   const { latitude, longitude } =
-    findNearest(conv.user.storage.location, vitalisLocations)
+    findNearest(coordinates, vitalisLocations)
   return locations.find(loc =>
     loc.latitude === latitude && loc.longitude === longitude
   )

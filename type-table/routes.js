46e6c93d1295b898459ac4779a1acede4d66d304
ElizaLU <ELIZA.LUSZCZEWSKA@GMAIL.com>
2019-12-05 17:41:00 +0100

    cleanup

--- type-table/routes.js
+++ type-table/routes.js
@@ -2,7 +2,31 @@ const { Router } = require('express')
 const Dishtype = require('./model')
 const router = new Router()
 
-//update a dish type
+//get all types
+router.get("/types", function (req, res, next) {
+  Dishtype.findAll()
+    .then(types => {
+      res.json(types);
+    })
+    .catch(err => next(err));
+});
+
+//gets a type by id
+router.get("/types/:id", function (req, res, next) {
+  const id = req.params.id;
+  Dishtype.findByPk(id)
+    .then(type => {
+      if (!type) {
+        return res.status(404).send({
+          message: `Type does not exit`
+        });
+      }
+      return res.send(type)
+    })
+    .catch(err => next(err))
+})
+
+//update a dish type NOT USED
 router.put('/types/:id', (req, res, next) => {
   Dishtype
     .findByPk(req.params.id)
@@ -17,7 +41,7 @@ router.put('/types/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-//delele a dish type
+//delele a dish type NOT USED
 router.delete('/types/:id', (req, res, next) => {
   Dishtype
     .findByPk(req.params.id)
@@ -35,7 +59,7 @@ router.delete('/types/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-// create new type
+// create new type NOT USED 
 router.post('/location/:locationId/types', function (req, res, next) {
   console.log('REQ', req.body)
   const type = {
@@ -51,32 +75,6 @@ router.post('/location/:locationId/types', function (req, res, next) {
       return res.send(type)
     })
     .catch(err => next(err));
-})
-
-//get all types
-router.get("/types", function (req, res, next) {
-
-  Dishtype.findAll()
-    .then(types => {
-      res.json(types);
-    })
-    .catch(err => next(err));
-});
-
-//gets a type by id
-router.get("/types/:id", function (req, res, next) {
-
-  const id = req.params.id;
-  Dishtype.findByPk(id)
-    .then(type => {
-      if (!type) {
-        return res.status(404).send({
-          message: `Type does not exit`
-        });
-      }
-      return res.send(type)
-    })
-    .catch(err => next(err))
 })
 
 module.exports = router
